068ed2a36d9298fef4c8f7bff6cef0b5599f0777
goldenamir <emir.taghizadeh@gmail.com>
2023-08-29 13:51:20 +0000

    minor modification

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-# importing required library
 import datetime
 import pytz
 import pandas as pd 
 import MetaTrader5 as mt5 
 import numpy as np
+
+# ... (rest of the code remains the same) ...
 
 # adding timeFrames
 time_frame = {
@@ -29,26 +30,22 @@ assets = {
     'ethrioum':'ETHUSD',
     'gold':'XAUUSD', 
     'silver':'XAGUSD', 
-    }
-
+}
 
 # timezone list
-timezone = ['America/Newyork', 'Europe/London', 'Europe/Paris', 'Asia/Tokyo', 'Australia/Sydney']
+timezone = ['America/New_York', 'Europe/London', 'Europe/Paris', 'Asia/Tokyo', 'Australia/Sydney']
 
-# function for getting qoutes
-
-def get_quotes(time_frame = time_frame['frame_H4'], year = 2023, month = 8, day = 18, asset = assets['gold']):
+# function for getting quotes
+def get_quotes(time_frame=time_frame['frame_H4'], year=2023, month=8, day=18, asset=assets['gold']):
     if not mt5.initialize():
         print("initialize() failed, error code =", mt5.last_error())
-
         quit()
 
-    timezone = pytz.timezone('Europe/Paris')
-    time_from = datetime.datetime(year, month, day, tzinfo = timezone)
-    time_to = datetime.datetime.now(tz = timezone) + datetime.timedelta(days = 1)
+    timezone_obj = pytz.timezone('Europe/Paris')
+    time_from = datetime.datetime(year, month, day, tzinfo=timezone_obj)
+    time_to = datetime.datetime.now(tz=timezone_obj) + datetime.timedelta(days=1)
     rates = mt5.copy_rates_range(asset, time_frame, time_from, time_to)
     rates_frame = pd.DataFrame(rates)
     return rates_frame
 
-
-print(get_quotes(time_frame=frame_H4,year=2023, month=8, day=25))
+print(get_quotes(time_frame=time_frame['frame_H4'], year=2023, month=8, day=25))
